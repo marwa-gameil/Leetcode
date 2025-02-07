e205b50c6121c74a9b2c219715a71af71ec2f74f
marwa-gameil <marwagamil666@gmail.com>
2025-02-07 16:35:15 +0200

    Majority Element

---
+++ Majority Element.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int size = nums.size();
+        sort(nums.begin(),nums.end());
+       
+        return nums[size/2];
+    }
+};
