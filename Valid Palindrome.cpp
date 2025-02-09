888093f3e98e2d53722d7f28ebf149550b494408
marwa-gameil <marwagamil666@gmail.com>
2025-02-09 14:10:15 +0200

    Valid Palindrome

---
+++ Valid Palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string filtered;
+         for (char c : s) {
+        if (isalnum(c)) {
+            filtered += tolower(c);
+        }
+    }
+    string reversed = filtered;
+    reverse(reversed.begin(), reversed.end());
+
+    return filtered == reversed;
+}
+    
+};
