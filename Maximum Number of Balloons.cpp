6d8b7a9fe9bdd3317f8c63f7e90a748b72a5e8b8
marwa-gameil <marwagamil666@gmail.com>
2025-02-23 16:58:04 +0200

    Hash/array/easy

---
+++ Maximum Number of Balloons.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        map<char,int>mp;
+        for(int i =0 ; i<text.size();i++)
+            mp[text[i]]++;
+
+        int b =mp['b'];
+        int a = mp['a'];
+        int l = mp['l']/2;
+        int o = mp['o']/2;
+        int n = mp['n'];
+
+            return min({b,a,l,o,n});
+        
+    }
+};
