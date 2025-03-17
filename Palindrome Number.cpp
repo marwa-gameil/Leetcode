5ff90e541fd504b425c0e59e3181f517f18f9e23
marwa-gameil <marwagamil666@gmail.com>
2025-03-18 00:30:03 +0200

    two pointers/easy

---
+++ Palindrome Number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       string str = to_string(x);
+       int low =0;
+       int high = str.length()-1;
+      while(low<high){
+        if(str[low] != str[high]){
+            return false;
+           
+        }
+         low++;
+        high--;
+
+      }
+     
+
+      return true;
+
+       
+    }
+};
