b7e216d7542739a07ed5102c18a6d860f16b61bf
marwa-gameil <marwagamil666@gmail.com>
2025-04-12 20:57:49 +0200

    medium

---
+++ Unique Length-3 Palindromic Subsequences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int first[26] = {[0 ... 25] = INT_MAX} 
+        ,last[26]={0}
+        ,res=0;
+
+        for(int i=0; i<s.size(); i++){
+            first[s[i]-'a'] = min(i,first[s[i]-'a']);
+            last[s[i]-'a'] =i;
+
+        }
+         for(int i=0; i<26; i++){
+            if(first[i] < last[i]){
+                res+= unordered_set<char>(begin(s)+first[i] +1, begin(s) +last[i]).size();
+            }
+         }
+    return res;
+    }
+};
