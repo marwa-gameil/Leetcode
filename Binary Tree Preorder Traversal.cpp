e0ab78c4f4dba0ef4b52b159a3f29f8e8404c857
marwa-gameil <marwagamil666@gmail.com>
2025-04-15 20:04:32 +0200

    tree/easy

---
+++ Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void help(TreeNode* root, vector<int> &v) {
+        if(!root) return;
+
+        v.push_back(root->val);
+        help(root->left,v);
+        help(root->right,v);
+
+    }
+    vector<int> preorderTraversal(TreeNode* root){
+        vector<int>v;
+        help(root,v);
+        return v;
+
+    }
+};
